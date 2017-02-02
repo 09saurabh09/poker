4bfe8716fee26a82747fb1c159ec582e41d47daa
Amar Nath Saha <amarnath.saha@olacabs.com>
2017-02-02 12:43:16 +0530

    added player

--- client/src/components/views/player/player.jsx
+++ client/src/components/views/player/player.jsx
@@ -5,9 +5,34 @@ export default class Player extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="player">
         <div className="player-container">
-        	
+          <div className="player-wrapper">
+            <div className="player-dp">
+              <img src="" />
+            </div>
+            <div className="player-name">Amar Nath Saha</div>
+            <div className="player-color-container">
+              <div className="color-dot">
+                
+              </div>
+            </div>
+            <div className="player-money">
+              <div className="player-balance">
+                $1234
+              </div>
+              <div className="player-bb">
+                BB <span className="bb-value"> 26 </span>
+              </div>
+            </div>
+            <div>
+              
+            </div>
+            <div className="player-game-style">
+              <div className="hands-played"></div>
+              <div className="raised"></div>
+            </div>
+          </div>
         </div>
       </div>
     );
