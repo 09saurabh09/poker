e83e7405b0232ba9e920136f6c631ed4feef494f
Amar Nath Saha <amarnath.saha@olacabs.com>
2017-02-03 12:59:20 +0530

    table created

--- client/src/components/views/game-table/game-table.jsx
+++ client/src/components/views/game-table/game-table.jsx
@@ -8,12 +8,30 @@ export default class GameTable extends React.Component{
 
   constructor(props) {
     super(props);
+    $(document).ready(function() {
+        $(window).resize(function() {
+          if($('.main-table').height() <= ($(window).height() * .65) ){
+            $('.main-table').width( $(window).width() * .75 );
+            $('.main-table').height( $(window).width() * .40 );
+          } else {
+            $('.main-table').height( $(window).height() * .70);
+            $('.main-table').width( $('.main-table').height() * 2 );
+          }
+        }).resize();
+    });
   }
 
   render() {
+    let allPlayers = Array.apply(null, Array(9));
     return (
-      <div>
-        <Player />
+      <div className="game-table">
+        <div className="main-table">
+           {allPlayers.map((element, index)=> 
+            <div key={index} className={'game-player ' + 'player' + index}>
+              <Player />
+            </div>
+            )}
+        </div>
       </div>
     );
   }
