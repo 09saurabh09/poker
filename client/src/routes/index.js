4055f433d47ea2415693d36ed3a49de38b6454b4
Amar Nath Saha <amarnath.saha@olacabs.com>
2017-03-01 00:49:10 +0530

    only home

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -4,30 +4,30 @@ import { Router, Route, IndexRoute } from 'react-router'
 import configureStore from '../store/configureStore'
 
 //import App from '../components/containers/App' 
-import LobyLayout from '../components/layouts/loby/loby-layout';
+//import LobyLayout from '../components/layouts/loby/loby-layout';
 
 // Pages
 import Home from '../components/containers/home/home-container.jsx';
-import CashGameContainer from '../components/containers/loby/cash-game/cash-game-container';
+/*import CashGameContainer from '../components/containers/loby/cash-game/cash-game-container';
 import TournamentContainer from '../components/containers/loby/tournament/tournament-container';
-import TableContainer from '../components/containers/game-table/table-container';
+import TableContainer from '../components/containers/game-table/table-container';*/
 
 export default function(history) {
   return (
     <Router history={history}>
       <Route path="/" component={Home} />
-		<Route path="/cash-game">
-			<Route component={LobyLayout} >
-				<IndexRoute component={CashGameContainer} />
-			</Route>
-			<Route path=":id" component={TableContainer} />	
+	{/*<Route path="/cash-game">
+		<Route component={LobyLayout} >
+			<IndexRoute component={CashGameContainer} />
 		</Route>
-		
-		<Route path="tournament">
-			<Route component={LobyLayout} >
-				<IndexRoute component={TournamentContainer} />
-			</Route>
+		<Route path=":id" component={TableContainer} />	
+	</Route>
+	
+	<Route path="tournament">
+		<Route component={LobyLayout} >
+			<IndexRoute component={TournamentContainer} />
 		</Route>
+	</Route>*/}
     </Router>
   )
 }
