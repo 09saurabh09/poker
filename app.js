0b14a37b1c704c6f328fcec90e82175de5f6e2af
Amar Nath Saha <amarnath.saha@olacabs.com>
2017-03-12 22:51:41 +0530

    images as urls

--- app.js
+++ app.js
@@ -71,9 +71,13 @@ app.use(compression())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'dist/public')))
+  app.use('/cash-game', express.static(path.join(__dirname, 'dist/public')))
+  app.use('/cash-game/play', express.static(path.join(__dirname, 'dist/public')))
 } else {
   app.use('/assets', express.static(path.join(__dirname, 'client/assets')))
   app.use(express.static(path.join(__dirname, 'dist')))
+  app.use('/cash-game/assets', express.static(path.join(__dirname, 'client/assets')))
+  app.use('/cash-game/play/assets', express.static(path.join(__dirname, 'client/assets')))
 }
 
 /*app.use(express.static(path.join(__dirname, 'build')));
